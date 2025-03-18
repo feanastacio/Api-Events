a46149c55b23cb34eea502508e93234e89a79903
Fernanda Marques <fernada.anastacio@aluno.senai.br>
2025-03-18 16:20:18 -0300

    feat: Concluindo o controller e Repository evento e tipodeevento

--- Controllers/EventoController.cs
+++ Controllers/EventoController.cs
@@ -1,11 +1,95 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Api_Event.Domains;
+using Api_Event.Interface;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api_Event.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Produces("application/json")]
     public class EventoController : ControllerBase
     {
+        private readonly IEventoRepository _eventoRepository;
+        public EventoController(IEventoRepository eventoRepository)
+        {
+            _eventoRepository = eventoRepository;
+        }
+
+        //Metodo Listar 
+        [HttpGet("{id}")]
+        public IActionResult Get()
+        {
+            try
+            {
+                List<Evento> listarEvento = _eventoRepository.Listar();
+                return Ok(listarEvento);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        //Metodo Cadastrar
+        [HttpPost]
+        public IActionResult Post(Evento novoEvento)
+        {
+            try
+            {
+                _eventoRepository.Cadastrar(novoEvento);
+                return Created();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        //Metodo Buscar
+        [HttpGet("BuscarPorId/{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            try
+            {
+                Evento eventoBuscado = _eventoRepository.BuscarPorId(id);
+                return Ok(eventoBuscado);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        //Metodo Deletar
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                _eventoRepository.Deletar(id);
+                return NoContent();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        //MetodoAtualizar
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, Evento evento)
+        {
+            try
+            {
+                _eventoRepository.Atualizar(id, evento);
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
     }
 }
--- Repositories/EventoRepository.cs
+++ Repositories/EventoRepository.cs
@@ -1,7 +1,16 @@
-﻿namespace Api_Event.Repositories
+﻿using Api_Event.Domains;
+
+namespace Api_Event.Repositories
 {
-    public class EventoRepository
+    public interface EventoRepository
     {
+            void Cadastrar(Evento novoEvento);
+            List<Evento> Listar();
+            void Atualizar(Guid id, Evento evento);
+            void Deletar(Guid id);
+            Evento BuscarPorId(Guid id);
+            //Listar os filmes pelo seu genero - filtro
+            List<Evento> ListarPorGenero(Guid IdGenero);
 
     }
 }
